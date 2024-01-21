b9c4eb4d5b46b7ce76bef30c8dcce62d66ee255f
kaede <micro_191ya@outlook.jp>
2024-01-21 22:14:39 +0900

    初期状態

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.demo;
+
+import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+        	
+        	
+        //.logout((logout) -> logout.permitAll())
+            .authorizeHttpRequests(requests -> requests
+            		.requestMatchers(PathRequest.toStaticResources().atCommonLocations().toString()).permitAll()
+            	    //.requestMatchers("/accountLogin").permitAll()
+            	    //.requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+           	    .anyRequest().authenticated()
+            )
+            
+            .formLogin(form -> form
+            	//.loginProcessingUrl("/accountLogin")
+                // ログインページへのパスを指定→コントローラーにもGET、/loginでの処理を記載する必要がある
+                .loginPage("/accountLogin")
+                // ログイン成功時に表示される画面へのパス
+                .defaultSuccessUrl("/")
+                .permitAll()
+            );
+            
+
+        return http.build();
+    }
+}
